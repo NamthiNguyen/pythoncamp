0a24ccc73ca537354803d050a0894a292818ac74
Namthi Nguyen <nnguye29@students,towson.edu>
2023-02-08 19:00:56 -0500

    first few basic codes

---
+++ evenorodd.py
@@ -0,0 +1,14 @@
+"""
+The Complete Python Pro Bootcamp for 2023
+Name: Namthi Nguyen
+Day 3 exercise #1: Even or odd
+
+"""
+
+
+numberforcheck = int(input("What number do you want to check? "))
+
+if numberforcheck % 2 == 0:
+    print(f"The number {numberforcheck} .. is a even number")
+else:
+    print(f"The number {numberforcheck} .. is a odd number")
