7a6f26994a74c05e6f6d6784f82d234648a7314e
Namthi Nguyen <nnguye29@students,towson.edu>
2023-02-08 19:02:46 -0500

    comments added

--- evenorodd.py
+++ evenorodd.py
@@ -5,9 +5,10 @@ Day 3 exercise #1: Even or odd
 
 """
 
-
+#this take an users input for number they want to check
 numberforcheck = int(input("What number do you want to check? "))
 
+# this if statement takes in the number and checks for remainder
 if numberforcheck % 2 == 0:
     print(f"The number {numberforcheck} .. is a even number")
 else:
